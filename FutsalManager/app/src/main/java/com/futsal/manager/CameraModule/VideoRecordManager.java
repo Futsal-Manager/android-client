257702988c081575bb31189a91aa0f8dab28c342
stories2 <stories2@naver.com>
2017-02-09 01:19:08 +0900

    CameraModule : ToggleButton Event Process
    toggleRecordVideo 이름의 토글 버튼이 눌림으로 인해onCheckedChanged 메소드 호출을 하여
    checkStatus의 값에 따라 if문 처리하여 log출력

--- FutsalManager/app/src/main/java/com/futsal/manager/CameraModule/VideoRecordManager.java
+++ FutsalManager/app/src/main/java/com/futsal/manager/CameraModule/VideoRecordManager.java
@@ -2,6 +2,9 @@ package com.futsal.manager.CameraModule;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import com.futsal.manager.R;
 
@@ -10,9 +13,25 @@ import com.futsal.manager.R;
  */
 
 public class VideoRecordManager extends Activity{
+    ToggleButton toggleRecordVideo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.record_video_surface);
+
+        toggleRecordVideo = (ToggleButton)findViewById(R.id.toggleRecordVideo);
+
+        toggleRecordVideo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checkStatus) {
+                if(checkStatus) {
+                    Log.d(getString(R.string.videoRecordManagerLogCatTag), "true");
+                }
+                else {
+                    Log.d(getString(R.string.videoRecordManagerLogCatTag), "false");
+                }
+            }
+        });
     }
 }
