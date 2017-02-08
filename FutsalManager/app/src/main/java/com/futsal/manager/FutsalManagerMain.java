7650dd5a21ebf5987c83260402cc92e5e2a17c4e
stories2 <stories2@naver.com>
2017-02-08 23:47:15 +0900

    기본 작업환경 셋팅
    프로젝트 생성 후 FutsalManagerMain이라는 클래스를 Main클래스로 하고, 그 레이아웃은
    futsal_manager_main.xml로 하였다

---
+++ FutsalManager/app/src/main/java/com/futsal/manager/FutsalManagerMain.java
@@ -0,0 +1,13 @@
+package com.futsal.manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FutsalManagerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.futsal_manager_main);
+    }
+}
