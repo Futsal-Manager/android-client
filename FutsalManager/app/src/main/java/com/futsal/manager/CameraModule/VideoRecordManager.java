e47418661d0b2ea2cf8c59056ea9be39803cfaa6
stories2 <stories2@naver.com>
2017-02-09 10:02:49 +0900

    CameraModule : add activity lifecycle method
    액티비티의 주요 생명주기 메소드마다 로그를 출력하여 현제 상태를 알 수 있게끔 함

--- FutsalManager/app/src/main/java/com/futsal/manager/CameraModule/VideoRecordManager.java
+++ FutsalManager/app/src/main/java/com/futsal/manager/CameraModule/VideoRecordManager.java
@@ -12,13 +12,16 @@ import com.futsal.manager.R;
  * Created by stories2 on 2017. 2. 9..
  */
 
-public class VideoRecordManager extends Activity{
+public class VideoRecordManager extends Activity {
+    String videoRecordManagerLogCatTag;
     ToggleButton toggleRecordVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.record_video_surface);
+        videoRecordManagerLogCatTag = getString(R.string.videoRecordManagerLogCatTag);
+        Log.d(videoRecordManagerLogCatTag, "onCreate");
 
         toggleRecordVideo = (ToggleButton)findViewById(R.id.toggleRecordVideo);
 
@@ -26,12 +29,48 @@ public class VideoRecordManager extends Activity{
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checkStatus) {
                 if(checkStatus) {
-                    Log.d(getString(R.string.videoRecordManagerLogCatTag), "true");
+                    Log.d(videoRecordManagerLogCatTag, "true");
                 }
                 else {
-                    Log.d(getString(R.string.videoRecordManagerLogCatTag), "false");
+                    Log.d(videoRecordManagerLogCatTag, "false");
                 }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(videoRecordManagerLogCatTag, "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(videoRecordManagerLogCatTag, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(videoRecordManagerLogCatTag, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(videoRecordManagerLogCatTag, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(videoRecordManagerLogCatTag, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(videoRecordManagerLogCatTag, "onDestroy");
     }
 }
