d0ff1ed7a62579582555200d6c8e6557d61796ff
stories2 <stories2@naver.com>
2017-02-14 03:02:42 +0900

    CameraModule : Init Device Camera and MediaRecorder
    MediaRecorder 객체에 HD화질의 24FPS영상을 MP4포멧으로 찍는다는걸 알려주고 촬영을 시작
    
    장치 카메라에 접근을 못하는 에러가 존재

--- FutsalManager/app/src/main/java/com/futsal/manager/CameraModule/VideoRecordManager.java
+++ FutsalManager/app/src/main/java/com/futsal/manager/CameraModule/VideoRecordManager.java
@@ -1,8 +1,12 @@
 package com.futsal.manager.CameraModule;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.CompoundButton;
@@ -15,9 +19,14 @@ import com.futsal.manager.R;
  */
 
 public class VideoRecordManager extends Activity implements SurfaceHolder.Callback{
+    final String videoSavePath = "/storage/emulated/DCIM/test.mp4";
+
     String videoRecordManagerLogCatTag;
     ToggleButton toggleRecordVideo;
     SurfaceView surfaceRecordVideo;
+    SurfaceHolder surfaceHolderRecordVideo;
+    Camera phoneDeviceCamera;
+    MediaRecorder deviceMediaRecorderManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +38,30 @@ public class VideoRecordManager extends Activity implements SurfaceHolder.Callba
         toggleRecordVideo = (ToggleButton)findViewById(R.id.toggleRecordVideo);
         surfaceRecordVideo = (SurfaceView)findViewById(R.id.surfaceRecordVideo);
 
+        RotateScreenLANDSCAPE();
+        surfaceHolderRecordVideo = InitSurfaceView(surfaceRecordVideo, surfaceHolderRecordVideo);
+
         toggleRecordVideo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checkStatus) {
                 if(checkStatus) {
                     Log.d(videoRecordManagerLogCatTag, "true");
+                    if(deviceMediaRecorderManager != null) {
+                        deviceMediaRecorderManager.start();
+                    }
                 }
                 else {
                     Log.d(videoRecordManagerLogCatTag, "false");
+                    if(deviceMediaRecorderManager != null) {
+                        deviceMediaRecorderManager.stop();
+                        deviceMediaRecorderManager.reset();
+                    }
+                    try {
+                        InitRecordVideo(surfaceHolderRecordVideo.getSurface(), phoneDeviceCamera, deviceMediaRecorderManager, videoSavePath);
+                    }
+                    catch (Exception err) {
+                        Log.d(videoRecordManagerLogCatTag, "Error in setOnCheckedChangeListener: " + err.getMessage());
+                    }
                 }
             }
         });
@@ -91,5 +116,46 @@ public class VideoRecordManager extends Activity implements SurfaceHolder.Callba
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
         Log.d(videoRecordManagerLogCatTag, "surfaceDestroyed");
+    }
+
+    public void RotateScreenLANDSCAPE() {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    public SurfaceHolder InitSurfaceView(SurfaceView targetSurfaceView, SurfaceHolder targetSurfaceHolder) {
+        targetSurfaceHolder = targetSurfaceView.getHolder();
+        targetSurfaceHolder.addCallback(this);
+        targetSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        return targetSurfaceHolder;
+    }
+
+    public boolean InitRecordVideo(Surface cameraViewSurface, Camera phoneCamera, MediaRecorder mediaRecorderManager, String pathOfStoreVideoFile) {
+        try {
+            if(phoneCamera == null) {
+                phoneCamera = Camera.open();
+                phoneCamera.unlock();
+            }
+
+            if(mediaRecorderManager == null) {
+                mediaRecorderManager = new MediaRecorder();
+            }
+            mediaRecorderManager.setPreviewDisplay(cameraViewSurface);
+            mediaRecorderManager.setCamera(phoneCamera);
+
+            mediaRecorderManager.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+            mediaRecorderManager.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            mediaRecorderManager.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mediaRecorderManager.setVideoEncodingBitRate(512 * 1000);
+            mediaRecorderManager.setVideoFrameRate(24);
+            mediaRecorderManager.setVideoSize(1280, 720);
+            mediaRecorderManager.setOutputFile(pathOfStoreVideoFile);
+
+            mediaRecorderManager.prepare();
+            return true;
+        }
+        catch (Exception err) {
+            Log.d(videoRecordManagerLogCatTag, "Error in InitRecordVideo: " + err.getMessage());
+        }
+        return false;
     }
 }
