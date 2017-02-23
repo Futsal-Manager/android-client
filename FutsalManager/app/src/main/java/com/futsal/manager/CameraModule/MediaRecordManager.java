e9d6e8dd0d41a2ad0df5507c4d1fd7e237198e62
stories2 <stories2@naver.com>
2017-02-24 02:16:05 +0900

    CameraModule : Save Video File Successfully
    뭔진 모르겠지만 일단 저장이 됬어!

--- FutsalManager/app/src/main/java/com/futsal/manager/CameraModule/MediaRecordManager.java
+++ FutsalManager/app/src/main/java/com/futsal/manager/CameraModule/MediaRecordManager.java
@@ -2,7 +2,9 @@ package com.futsal.manager.CameraModule;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -23,24 +25,58 @@ public class MediaRecordManager extends Activity implements SurfaceHolder.Callba
     ToggleButton toggleRecordVideo;
     SurfaceView surfaceRecordVideo;
     SurfaceHolder surfaceHolderRecordVideo;
+    MediaRecorder mediaRecording;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.record_video_surface_based_on_opencv);
+        setContentView(R.layout.record_media_surface);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         toggleRecordVideo = (ToggleButton) findViewById(R.id.toogleRecordVideo);
         surfaceRecordVideo = (SurfaceView) findViewById(R.id.surfaceRecordVideo);
 
+        surfaceHolderRecordVideo = surfaceRecordVideo.getHolder();
+        surfaceHolderRecordVideo.addCallback(this);
+        surfaceHolderRecordVideo.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
         toggleRecordVideo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked) {
+                    if(mediaRecording == null) {
+                        String savePath = Environment.getExternalStorageDirectory().toString();
+                        mediaRecording = new MediaRecorder();
+                        mediaRecording.setAudioSource(MediaRecorder.AudioSource.MIC);
+                        mediaRecording.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                        mediaRecording.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                        mediaRecording.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                        mediaRecording.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
+
+                        mediaRecording.setPreviewDisplay(surfaceHolderRecordVideo.getSurface());
+                        mediaRecording.setOutputFile(savePath + "/testVideo2.mp4");
+
+                        try {
+                            mediaRecording.prepare();
+                            mediaRecording.start();
+                        }
+                        catch (Exception err) {
+                            Log.d(mediaRecordLogCatTag, "Error in setOnCheckedChangeListener: " + err.getMessage());
+                            mediaRecording.release();
+                            mediaRecording = null;
+                        }
+                    }
                     Log.d(mediaRecordLogCatTag, "checked");
                 }
                 else {
+                    if(mediaRecording == null) {
+                        return;
+                    }
+                    mediaRecording.stop();
+                    mediaRecording.reset();
+                    mediaRecording.release();
+                    mediaRecording = null;
                     Log.d(mediaRecordLogCatTag, "unchecked");
                 }
             }
