8b392f869edde3168d003c0ecbb5559899b5b886
Ubuntu <ubuntu@ip-172-31-14-209.ap-northeast-2.compute.internal>
2017-03-02 17:35:16 +0000

    editted app/build.gradle ndk

--- openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/stories2/Desktop/FutsalManager/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/ubuntu/android-client/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
