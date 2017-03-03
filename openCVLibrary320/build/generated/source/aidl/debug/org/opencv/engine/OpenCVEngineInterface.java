4dba515b33bb15e6c9b5af2d985b2116af23b0bf
stories2 <stories2@naver.com>
2017-03-04 08:46:11 +0900

    Retrofit Library Added to Gradle
    인터넷 네트워크 연동을 위해 오픈소스 라이브러리 중 retrofit을 사용하기로 결정

--- openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\stori\\Desktop\\android-client\\FutsalManager\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /Users/stories2/android-client/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
