74eb1a60fc5a76518ae45a41466cd2435b027ef5
stories2 <stories2@naver.com>
2017-04-28 19:11:55 +0900

    Status Bar Color Updated
    액션바 배경 색깔과 상태바 배경 색깔을 통일시 하는 기능을 구현함

--- app/src/main/java/com/futsal/manager/FutsalManagerMain.java
+++ app/src/main/java/com/futsal/manager/FutsalManagerMain.java
@@ -1,15 +1,14 @@
 package com.futsal.manager;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Window;
 
 import com.futsal.manager.DataModelModule.EachCardViewItems;
 import com.futsal.manager.DataModelModule.RecyclerAdapter;
-import com.futsal.manager.LogModule.LogManager;
-
-import org.opencv.android.OpenCVLoader;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,9 +21,7 @@ public class FutsalManagerMain extends AppCompatActivity {
     RecyclerView.Adapter recyFunctionListAdapter;
     RecyclerView.LayoutManager recyFunctionListLayoutManager;
     List<EachCardViewItems> eachCardViewItems;
-
-
-
+    Window futsalManagerMainWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +29,9 @@ public class FutsalManagerMain extends AppCompatActivity {
         setContentView(R.layout.futsal_manager_main);
 
         recyFunctionList = (RecyclerView) findViewById(R.id.recyFunctionList);
+
+        futsalManagerMainWindow = getWindow();
+        futsalManagerMainWindow.setStatusBarColor(ContextCompat.getColor(this, R.color.futsalMainStatusBar));
 
         recyFunctionList.setHasFixedSize(true);
         recyFunctionListLayoutManager = new LinearLayoutManager(this);
