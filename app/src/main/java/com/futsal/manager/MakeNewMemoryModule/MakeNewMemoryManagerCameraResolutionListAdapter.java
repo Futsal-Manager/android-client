3ba6fdc4fc46742597044a53e96439916757b8bb
stories2 <stories2@naver.com>
2017-06-22 17:22:43 +0900

    Camera Module : Custom resolution list view

--- app/src/main/java/com/futsal/manager/MakeNewMemoryModule/MakeNewMemoryManagerCameraResolutionListAdapter.java
+++ app/src/main/java/com/futsal/manager/MakeNewMemoryModule/MakeNewMemoryManagerCameraResolutionListAdapter.java
@@ -1,15 +1,25 @@
 package com.futsal.manager.MakeNewMemoryModule;
 
+import android.content.Context;
 import android.database.DataSetObserver;
+import android.hardware.Camera;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.futsal.manager.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by stories2 on 2017. 6. 22..
  */
 
 public class MakeNewMemoryManagerCameraResolutionListAdapter extends BaseAdapter {
+
+    ArrayList<MakeNewMemoryManagerCameraResolutionListItem> cameraResolutionList = new ArrayList<MakeNewMemoryManagerCameraResolutionListItem>();
 
     public MakeNewMemoryManagerCameraResolutionListAdapter() {
         super();
@@ -22,7 +32,37 @@ public class MakeNewMemoryManagerCameraResolutionListAdapter extends BaseAdapter
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        MakeNewMemoryManagerCameraResolutionListItem indexOfScreenResoltuion;
+        indexOfScreenResoltuion = cameraResolutionList.get(position);
+
+        Context context = parent.getContext();
+
+        if(convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.make_new_memory_manager_camera_resolution_list_item, parent, false);
+        }
+
+        TextView txtVideoResolution = (TextView) convertView.findViewById(R.id.txtVideoResolution);
+        String textOfScreenResolution = CameraSizeToString(indexOfScreenResoltuion.GetAvailableCameraVideoRecordResolution());
+
+        txtVideoResolution.setText(textOfScreenResolution);
+
+        if(indexOfScreenResoltuion.GetIsSelected() == true) {
+            txtVideoResolution.setTextColor(context.getResources().getColor(R.color.dirtyRed));
+        }
+        else {
+            txtVideoResolution.setTextColor(context.getResources().getColor(R.color.whiteGray));
+        }
+
+        return convertView;
+    }
+
+    String CameraSizeToString(Camera.Size availableScreenRecordResolution) {
+        if(availableScreenRecordResolution != null) {
+            return "" + availableScreenRecordResolution.width + " X " + availableScreenRecordResolution.height;
+        }
+        return "Error!";
     }
 
     @Override
@@ -37,17 +77,17 @@ public class MakeNewMemoryManagerCameraResolutionListAdapter extends BaseAdapter
 
     @Override
     public int getCount() {
-        return 0;
+        return cameraResolutionList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return cameraResolutionList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
@@ -88,5 +128,13 @@ public class MakeNewMemoryManagerCameraResolutionListAdapter extends BaseAdapter
     @Override
     public boolean isEmpty() {
         return super.isEmpty();
+    }
+
+    public void AddItem(Camera.Size availableCameraVideoRecordResolution, boolean isSelected) {
+        MakeNewMemoryManagerCameraResolutionListItem makeNewMemoryManagerCameraResolutionListItem;
+        makeNewMemoryManagerCameraResolutionListItem = new MakeNewMemoryManagerCameraResolutionListItem();
+
+        makeNewMemoryManagerCameraResolutionListItem.SetAvailableCameraVideoRecordResolution(availableCameraVideoRecordResolution);
+        makeNewMemoryManagerCameraResolutionListItem.SetIsSelected(isSelected);
     }
 }
